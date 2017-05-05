a02e0b6b96713922f066cf2f3bd244acb0b04d92
antonio <prodoson@gmail.com>
2017-05-05 03:02:30 -0400

    Initial commit

---
+++ modules/test.py
@@ -0,0 +1,9 @@
+from modules.getWeights import *
+
+account = Weights('76561198014207004')
+print(account.getGameCount())
+print(account.getTotalPrice())
+print(account.getPennyWeight())
+print(account.getPhysicalWeight())
+print(account.getTotalValue())
+print(account.getAveragePrice())
